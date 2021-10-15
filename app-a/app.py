fa0c00c1d12bd3c2c817397812afec7e6ee6ba87
Luke A. Parker <lparker@PP-MBP-02.local>
2021-10-14 23:09:16 -0700

    init

---
+++ app-a/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from os import environ
+
+# Initilize appb
+app = Flask(__name__)
+
+# Appb landing page
+@app.route("/", methods=['GET'])
+def appa():
+    return 'Welcome to Appa!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('FLASK_RUN_PORT'))
